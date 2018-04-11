38707c93253715adb38bd96c620ec4cb69d83344
ke_ix1 <xkei4dev@gmail.com>
2018-04-12 07:44:31 +0900

    add bluetooth connection files but it is not work

---
+++ Assets/LedController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LedController : MonoBehaviour
+{
+    public SerialHandler serialHandler;
+
+    void Start() {
+
+    }
+
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.A) ) {
+            serialHandler.Write("0");
+        }
+        if ( Input.GetKeyDown(KeyCode.S) ) {
+            serialHandler.Write("1");
+        }
+    }
+}
